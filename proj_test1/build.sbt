9256584600b5de6d3e3d3f26ce110c1041c7f224
Shrivastava, Sunit(ss372n) <ss372n@att.com>
2019-01-31 14:08:16 -0600

    deleted proj_test1

--- proj_test1/build.sbt
+++
@@ -1,1 +0,0 @@
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0"
--- proj_test1/src/main/scala/HelloWorld.scala
+++
@@ -1,7 +0,0 @@
-package main.scala
-
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
-}
